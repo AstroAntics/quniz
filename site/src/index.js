65ba647c15a0e43cccf96c47221b239bd9b118f7
AstroAntics <vucinicm@asbindia.org>
2023-02-05 11:27:48 +0200

    latest

--- site/src/index.js
+++ site/src/index.js
@@ -13,7 +13,7 @@ import NewHomePage from "./NewHomepage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <NewHomePage />,
     errorElement: <ErrorPage />,
   },
   {
@@ -50,7 +50,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}>
-      <App />
+      <NewHomePage />
     </RouterProvider>
   </React.StrictMode>
 );
