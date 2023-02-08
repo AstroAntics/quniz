791730375b431833bb4a275302e3cdcec51be222
AstroAntics <vucinicm@asbindia.org>
2023-02-08 13:06:06 +0200

    Fix about us page

---
+++ site/src/pages/QuizPage.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material";
+
+export default function QuizPage() {
+    return(
+        <div id="quizPage">
+            <Container maxWidth="xl">
+                
+            </Container>
+        </div>
+    );
+}
